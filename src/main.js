53bc797da420ce3f56668a72ca7b3524578ba60b
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2019-07-05 14:58:33 +0530

    Get all tests passed

--- src/main.js
+++ src/main.js
@@ -1,28 +1,26 @@
 import Banana from 'banana-i18n'
 
 export default {
-  install: function (Vue, messages = {}) {
-    const banana = new Banana()
-    banana.load(messages)
-
-    const directive = {
-      params: ['key', 'params'],
-      bind: (el, binding) => {
-        banana.setLocale(binding.value)
-      },
-      update: (el) => {
-        el.innerText = this.vm.$i18n(this.params.key, this.params.params)
-      }
-    }
-
-    Vue.directive('locale', directive)
+  install (Vue, options = {
+    messages: {},
+    locale: 'en'
+  }) {
+    const banana = new Banana(options.locale, options)
     Vue.prototype.$i18n = (msgKey, ...params) => {
-      // const locale = this.$root.locale || navigator.language || navigator.browserLanguage
-      banana.setLocale('en')
       return banana.i18n(msgKey, ...params)
     }
-    Vue.filter('i18n', (msgKey, ...params) => {
-      this.$i18n(msgKey, ...params)
-    })
+
+    Vue.prototype.setLocale = (locale) => {
+      banana.setLocale(locale)
+    }
+    /**
+     * Load localized messages for a locale
+     * If locale not provided, the keys in messageSource will be used as locales.
+     * @param {Object} messageSource
+     * @param {string} [locale]
+     */
+    Vue.prototype.loadMessages = (messages, locale) => {
+      banana.load(messages, locale)
+    }
   }
 }
--- test/vue-banana.test.js
+++ test/vue-banana.test.js
@@ -9,10 +9,18 @@ const translations = {
     'hello_world': 'Hello world',
     'search_results': 'Found $1 {{PLURAL:$1|result|results}}',
     'profile_change_message': '$1 changed {{GENDER:$2|his|her}} profile picture'
+  },
+  ml: {
+    'hello_world': 'എല്ലാവർക്കും നമസ്കാരം',
+    'search_results': '{{PLURAL:$1|$1 ഫലം|$1 ഫലങ്ങൾ|1=ഒരു ഫലം}} കണ്ടെത്തി',
+    'profile_change_message': '$1 {{GENDER:$2|അവന്റെ|അവളുടെ}} പ്രൊഫൈൽ പടം മാറ്റി'
   }
 }
 
-Vue.use(VueBananai18n, translations)
+Vue.use(VueBananai18n, {
+  messages: translations,
+  locale: 'en'
+})
 
 describe('Vue-Banana-i18n', () => {
   it('will simply return the original string if no translation is found', () => {
@@ -20,14 +28,14 @@ describe('Vue-Banana-i18n', () => {
 
     assert.strict.equal(vm.$i18n('hello_world'), 'Hello world')
 
-    vm.$root.locale = 'DNE'
+    vm.setLocale('DNE')
     assert.strict.equal(vm.$i18n('msg_key_dne'), 'msg_key_dne')
   })
 
   it('will return the translation with placeholder substitutions', () => {
     let vm = new Vue()
 
-    vm.$root.locale = 'en'
+    vm.setLocale('en')
     assert.strict.equal(vm.$i18n('search_results', 0), 'Found 0 results')
     assert.strict.equal(vm.$i18n('search_results', 1), 'Found 1 result')
     assert.strict.equal(vm.$i18n('search_results', 10), 'Found 10 results')
@@ -39,5 +47,43 @@ describe('Vue-Banana-i18n', () => {
       vm.$i18n('profile_change_message', 'Bob', 'male'),
       'Bob changed his profile picture'
     )
+  })
+
+  it('will return the translation in correct locale when locale changed in between', () => {
+    let vm = new Vue()
+
+    vm.setLocale('en')
+    assert.strict.equal(vm.$i18n('search_results', 1), 'Found 1 result')
+    assert.strict.equal(vm.$i18n('search_results', 10), 'Found 10 results')
+    assert.strict.equal(
+      vm.$i18n('profile_change_message', 'Alice', 'female'),
+      'Alice changed her profile picture'
+    )
+    assert.strict.equal(
+      vm.$i18n('profile_change_message', 'Bob', 'male'),
+      'Bob changed his profile picture'
+    )
+    vm.setLocale('ml')
+    assert.strict.equal(vm.$i18n('search_results', 1), 'ഒരു ഫലം കണ്ടെത്തി')
+    assert.strict.equal(vm.$i18n('search_results', 10), '10 ഫലങ്ങൾ കണ്ടെത്തി')
+    assert.strict.equal(
+      vm.$i18n('profile_change_message', 'Alice', 'female'),
+      'Alice അവളുടെ പ്രൊഫൈൽ പടം മാറ്റി'
+    )
+    assert.strict.equal(
+      vm.$i18n('profile_change_message', 'Bob', 'male'),
+      'Bob അവന്റെ പ്രൊഫൈൽ പടം മാറ്റി'
+    )
+  })
+
+  it('will fallback to another locale if message is not present', () => {
+    let vm = new Vue()
+
+    vm.setLocale('en')
+    vm.loadMessages({
+      'new_mssage': 'New message'
+    }, 'en')
+    vm.setLocale('ml')
+    assert.strict.equal(vm.$i18n('new_mssage'), 'New message', 'Fallback to English')
   })
 })
