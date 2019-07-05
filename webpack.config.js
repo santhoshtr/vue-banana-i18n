55a32043c1e74edea1d10f8578afc050a2eac118
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2019-07-05 16:25:05 +0530

    Fix the build

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   entry: './src/main.js',
   output: {
-    library: 'Vue-Banana-i18n',
+    library: 'vue-banana-i18n',
     libraryExport: 'default',
     libraryTarget: 'umd',
     filename: 'vue-banana-i18n.js',
