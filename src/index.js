61e40ba13951715811f245acb40f928ef6b6f4c3
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2022-01-21 16:24:03 +0530

    Use vitest for testing, along with vite for build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Banana from 'banana-i18n'
-import { reactive } from 'vue'
+import { reactive, inject } from 'vue'
 const parseValue = (binding) => {
   let msg, params
 
@@ -22,46 +22,57 @@ const parseValue = (binding) => {
   }
 
   if (!msg) {
-    throw new Error(`${binding.rawName} used with parameter array but without message key`)
+    throw new Error(
+      `${binding.rawName} used with parameter array but without message key`
+    )
   }
 
   return { msg, params }
 }
 
-export default {
-  install: (app, options = { messages: {}, locale: 'en' }) => {
-    let bananai18n
-    app.mixin({
-      beforeCreate () {
-        // Create a reactive i18n instance so that locales can be
-        // changed on the go.
-        bananai18n = reactive(new Banana(options.locale, options))
+const contextSymbol = Symbol('banana-context')
+
+export function useI18n () {
+  const i18n = inject(contextSymbol)
+  if (!i18n) throw new Error('No i18n provided!!!')
+
+  return i18n
+}
+
+export function createI18n (options = { messages: {}, locale: 'en', wikilinks: true }) {
+  // Create a reactive i18n instance so that locales can be
+  // changed on the go.
+  const bananai18n = reactive(new Banana(options.locale, options))
+
+  return {
+    install: (app) => {
+      app.provide(contextSymbol, bananai18n)
+
+      app.config.globalProperties.$i18n = (msg, params) => {
+        return bananai18n.i18n(msg, params)
       }
-    })
 
-    app.config.globalProperties.$i18n = (msg, params) => {
-      return bananai18n.i18n(msg, params)
-    }
+      app.provide('setLocale', (newLocale) => {
+        bananai18n.setLocale(newLocale)
+      })
 
-    app.provide('setLocale', (newLocale) => {
-      bananai18n.setLocale(newLocale)
-    })
+      app.directive('i18n', (el, binding) => {
+        // this will be called as `mounted` and `updated`
+        const { msg, params } = parseValue(binding)
 
-    app.directive('i18n', (el, binding) => {
-      // this will be called as `mounted` and `updated`
-      const { msg, params } = parseValue(binding)
-      if (binding.modifiers.html) {
-        // v-i18n.html or v-i18n:message_key.html
+        if (binding.modifiers.html) {
+          // v-i18n.html or v-i18n:message_key.html
+          el.innerHTML = bananai18n.i18n(msg, params)
+        } else {
+          el.textContent = bananai18n.i18n(msg, params)
+        }
+      })
+
+      app.directive('i18n-html', (el, binding) => {
+        const { msg, params } = parseValue(binding)
+        // The content is sanitized HTML, hence innerHTML is safe.
         el.innerHTML = bananai18n.i18n(msg, params)
-      } else {
-        el.textContent = bananai18n.i18n(msg, params)
-      }
-    })
-
-    app.directive('i18n-html', (el, binding) => {
-      const { msg, params } = parseValue(binding)
-      // The content is sanitized HTML, hence innerHTML is safe.
-      el.innerHTML = bananai18n.i18n(msg, params)
-    })
+      })
+    }
   }
 }
---
+++ vite.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  test: {
+    environment: 'jsdom'
+  },
+  build: {
+    // generate manifest.json in outDir
+    manifest: true,
+    minify: 'esbuild',
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.js'),
+      name: 'vue-banana-i18n',
+      fileName: (format) => `vue-banana-i18n.${format}.js`
+    }
+  },
+  rollupOptions: {
+    // make sure to externalize deps that shouldn't be bundled
+    // into your library
+    external: ['vue'],
+    output: {
+      // Provide global variables to use in the UMD build
+      // for externalized deps
+      globals: {
+        vue: 'Vue'
+      }
+    }
+  }
+})
