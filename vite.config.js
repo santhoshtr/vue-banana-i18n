a5a8fdfe898003bb5c03cb8bf45f60c8bac511ed
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2022-03-14 14:53:00 +0530

    Fix the conf for externalizing vue, version bump
    Vue was getting bundled because of wrong vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -13,18 +13,19 @@ module.exports = defineConfig({
       entry: path.resolve(__dirname, 'src/index.js'),
       name: 'vue-banana-i18n',
       fileName: (format) => `vue-banana-i18n.${format}.js`
-    }
-  },
-  rollupOptions: {
-    // make sure to externalize deps that shouldn't be bundled
-    // into your library
-    external: ['vue'],
-    output: {
-      // Provide global variables to use in the UMD build
-      // for externalized deps
-      globals: {
-        vue: 'Vue'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue', 'reactive', 'inject'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
       }
     }
   }
+
 })
