a2a4ae2e577f1810528bf9d3707a05d6a402c4bc
Sergio Gimeno <sgimeno@wikimedia.org>
2023-05-26 14:43:53 +0200

    Rename ES module to .mjs
    Bug: #23

--- vite.config.js
+++ vite.config.js
@@ -11,8 +11,8 @@ module.exports = defineConfig({
     minify: 'esbuild',
     lib: {
       entry: path.resolve(__dirname, 'src/index.js'),
-      name: 'vue-banana-i18n',
-      fileName: (format) => `vue-banana-i18n.${format}.js`
+      name: 'vue-banana-i18n'
+      // fileName not specified; it'll be vue-banana-i18n.mjs or vue-banana-i18n.umd.js
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
